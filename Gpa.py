516ba78731da09fd5783cafdd65cb49a9bafe3d8
sobiamanha2023 <sobiamanha1@gmail.com>
2025-08-09 11:54:32 +0800

    Add files via upload

---
+++ Gpa.py
@@ -0,0 +1,22 @@
+marks = int(input("Enter your marks: "))
+
+if 80 <= marks <= 100:
+    print("A+")
+
+elif 70 <= marks <= 79:
+    print("A")
+
+elif 60 <= marks <= 69:
+    print("A-")
+
+elif 50 <= marks <= 59:
+    print("B")
+
+elif 40 <= marks <= 49:
+    print("C")
+
+elif 30 <= marks <= 39:
+    print("D")
+
+else:
+    print("F")
